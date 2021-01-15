1486d761061308a2ded5c6eedc21703670701cc3
9A713023 <73283855+9A713023@users.noreply.github.com>
2021-01-16 03:06:14 +0800

    Add files via upload

---
+++ WP_2020_01_HW02_9A713023/WP_2020_01_HW02_9A713023/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WP_2020_01_HW02_9A713023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGo.Font = new System.Drawing.Font("微軟正黑體", 18.33962F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonGo.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.buttonGo.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.buttonGo.Location = new System.Drawing.Point(323, 366);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(87, 57);
+            this.buttonGo.TabIndex = 1;
+            this.buttonGo.Text = "抽牌";
+            this.buttonGo.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(250, 115);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(229, 228);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picResult.TabIndex = 2;
+            this.picResult.TabStop = false;
+            this.picResult.Click += new System.EventHandler(this.pokerBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(218, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 67);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "撲克牌抽牌";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.buttonGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WP_2020_01_HW02_9A713023/WP_2020_01_HW02_9A713023/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_9A713023.Properties;
+
+namespace WP_2020_01_HW02_9A713023
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._001);
+            list.Add(Resources._002);
+            list.Add(Resources._003);
+            list.Add(Resources._004);
+            list.Add(Resources._2C);
+            list.Add(Resources._2D);
+            list.Add(Resources._2H);
+            list.Add(Resources._2S);
+            list.Add(Resources._3C);
+            list.Add(Resources._3D);
+            list.Add(Resources._3H);
+            list.Add(Resources._3S);
+            list.Add(Resources._4C);
+            list.Add(Resources._4D);
+            list.Add(Resources._4H);
+            list.Add(Resources._4S);
+            list.Add(Resources._5C);
+            list.Add(Resources._5D);
+            list.Add(Resources._5H);
+            list.Add(Resources._5S);
+            list.Add(Resources._6C);
+            list.Add(Resources._6D);
+            list.Add(Resources._6H);
+            list.Add(Resources._6S);
+            list.Add(Resources._7C);
+            list.Add(Resources._7D);
+            list.Add(Resources._7H);
+            list.Add(Resources._7S);
+            list.Add(Resources._8C);
+            list.Add(Resources._8D);
+            list.Add(Resources._8H);
+            list.Add(Resources._8S);
+            list.Add(Resources._9C);
+            list.Add(Resources._9D);
+            list.Add(Resources._9H);
+            list.Add(Resources._9S);
+            list.Add(Resources._10C);
+            list.Add(Resources._10D);
+            list.Add(Resources._10H);
+            list.Add(Resources._10S);
+            list.Add(Resources.JC);
+            list.Add(Resources.JD);
+            list.Add(Resources.JH);
+            list.Add(Resources.JS);
+            list.Add(Resources.QC);
+            list.Add(Resources.QD);
+            list.Add(Resources.QH);
+            list.Add(Resources.QS);
+            list.Add(Resources.KC);
+            list.Add(Resources.KD);
+            list.Add(Resources.KH);
+            list.Add(Resources.KS);
+      
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            poker poker1 = new poker();
+            List<int> poker = poker1.GetPoker(52);
+
+            string msg = "";
+            for (int i = 0; i < poker.Count; i++)
+            {
+                msg += $"{poker[i]} ,";
+            }
+            picResult.Image = list[poker[0]];
+           
+        }
+
+        
+        private void pokerBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
